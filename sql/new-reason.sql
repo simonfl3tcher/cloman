e1ed24c12704ac38284d102b42f81fc9ff2e34ee
Simon Fletcher <Macbook>
2012-09-11 18:57:23 +0100

    new sql to make it work added

---
+++ sql/new-reason.sql
@@ -0,0 +1,219 @@
+-- --------------------------------------------------------
+-- Host:                         
+-- Server version:               5.5.16 - MySQL Community Server (GPL)
+-- Server OS:                    Win32
+-- HeidiSQL version:             7.0.0.4053
+-- Date/time:                    2012-09-11 18:46:58
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+
+-- Dumping structure for table my.company.address
+DROP TABLE IF EXISTS `address`;
+CREATE TABLE IF NOT EXISTS `address` (
+  `Address_ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `Address_Line_1` varchar(150) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `Address_Line_2` varchar(150) COLLATE utf8_unicode_ci DEFAULT '',
+  `Address_Line_3` varchar(150) COLLATE utf8_unicode_ci DEFAULT '',
+  `City` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `Region_Name` varchar(100) COLLATE utf8_unicode_ci DEFAULT '',
+  `Postcode` varchar(10) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`Address_ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Dumping data for table my.company.address: 1 rows
+DELETE FROM `address`;
+/*!40000 ALTER TABLE `address` DISABLE KEYS */;
+INSERT INTO `address` (`Address_ID`, `Address_Line_1`, `Address_Line_2`, `Address_Line_3`, `City`, `Region_Name`, `Postcode`) VALUES
+	(1, '2 Gelbe House', 'Woolpit', 'Rattlesden', 'Stowmarket', '', 'IP29 1LD');
+/*!40000 ALTER TABLE `address` ENABLE KEYS */;
+
+
+-- Dumping structure for table my.company.businesses
+DROP TABLE IF EXISTS `businesses`;
+CREATE TABLE IF NOT EXISTS `businesses` (
+  `business_id` int(10) NOT NULL AUTO_INCREMENT,
+  `address_id` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`business_id`),
+  KEY `business_id` (`business_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table my.company.businesses: ~3 rows (approximately)
+DELETE FROM `businesses`;
+/*!40000 ALTER TABLE `businesses` DISABLE KEYS */;
+INSERT INTO `businesses` (`business_id`, `address_id`, `name`, `phone`, `email`) VALUES
+	(1, NULL, 'Logic Design', '01284706842', 'hello@logicdesign.co.uk'),
+	(2, NULL, 'Big Earth', '0207 657 2727', 'office@bigearth.co.uk'),
+	(3, NULL, 'Nicola Sexton', '01284 760011', 'info@nicolasexton.co.uk');
+/*!40000 ALTER TABLE `businesses` ENABLE KEYS */;
+
+
+-- Dumping structure for table my.company.connections
+DROP TABLE IF EXISTS `connections`;
+CREATE TABLE IF NOT EXISTS `connections` (
+  `connection_id` int(10) NOT NULL AUTO_INCREMENT,
+  `website_id` int(10) DEFAULT NULL,
+  `connection_options_id` int(11) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `ip` varchar(50) DEFAULT NULL,
+  `notes` text,
+  PRIMARY KEY (`connection_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table my.company.connections: ~0 rows (approximately)
+DELETE FROM `connections`;
+/*!40000 ALTER TABLE `connections` DISABLE KEYS */;
+/*!40000 ALTER TABLE `connections` ENABLE KEYS */;
+
+
+-- Dumping structure for table my.company.connection_options
+DROP TABLE IF EXISTS `connection_options`;
+CREATE TABLE IF NOT EXISTS `connection_options` (
+  `connection_options_id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`connection_options_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table my.company.connection_options: ~0 rows (approximately)
+DELETE FROM `connection_options`;
+/*!40000 ALTER TABLE `connection_options` DISABLE KEYS */;
+/*!40000 ALTER TABLE `connection_options` ENABLE KEYS */;
+
+
+-- Dumping structure for table my.company.people
+DROP TABLE IF EXISTS `people`;
+CREATE TABLE IF NOT EXISTS `people` (
+  `people_id` int(10) NOT NULL AUTO_INCREMENT,
+  `business_id` int(10) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `is_primary_contact` enum('Y','N') DEFAULT 'N',
+  `notes` text,
+  PRIMARY KEY (`people_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table my.company.people: ~3 rows (approximately)
+DELETE FROM `people`;
+/*!40000 ALTER TABLE `people` DISABLE KEYS */;
+INSERT INTO `people` (`people_id`, `business_id`, `name`, `role`, `email`, `phone`, `is_primary_contact`, `notes`) VALUES
+	(18, 16, 'Shaun Palfrey', 'Medical Records Manager', 'shaun@medrecs.co.uk', '120165578', 'N', NULL),
+	(19, 17, 'Simon Fletcher', 'Website Developer', 'simon@logicdesign.co.uk', '7', 'N', NULL),
+	(21, 19, 'Paul Fletcher', 'Managing Director', 'paul@apestpro.co.uk', '10', 'N', NULL);
+/*!40000 ALTER TABLE `people` ENABLE KEYS */;
+
+
+-- Dumping structure for table my.company.projects
+DROP TABLE IF EXISTS `projects`;
+CREATE TABLE IF NOT EXISTS `projects` (
+  `project_id` int(10) NOT NULL AUTO_INCREMENT,
+  `business_id` varchar(255) DEFAULT NULL,
+  `project_manager` varchar(255) DEFAULT NULL,
+  `status` enum('Y','N') DEFAULT NULL,
+  `project_type` varchar(255) DEFAULT NULL,
+  `project_people` varchar(255) DEFAULT NULL,
+  `project_users` varchar(255) DEFAULT NULL,
+  `client_deadline` datetime DEFAULT NULL,
+  `budget` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`project_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table my.company.projects: ~0 rows (approximately)
+DELETE FROM `projects`;
+/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
+/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
+
+
+-- Dumping structure for table my.company.support_packs
+DROP TABLE IF EXISTS `support_packs`;
+CREATE TABLE IF NOT EXISTS `support_packs` (
+  `support_packs_id` int(10) NOT NULL AUTO_INCREMENT,
+  `description` varchar(50) DEFAULT NULL,
+  `includes` text,
+  `time_allowed_pm` int(11) DEFAULT NULL,
+  PRIMARY KEY (`support_packs_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table my.company.support_packs: ~0 rows (approximately)
+DELETE FROM `support_packs`;
+/*!40000 ALTER TABLE `support_packs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `support_packs` ENABLE KEYS */;
+
+
+-- Dumping structure for table my.company.support_packs_to_businesses
+DROP TABLE IF EXISTS `support_packs_to_businesses`;
+CREATE TABLE IF NOT EXISTS `support_packs_to_businesses` (
+  `sptb_id` int(10) NOT NULL AUTO_INCREMENT,
+  `businesses_id` int(10) DEFAULT NULL,
+  `support_pack_id` int(10) DEFAULT NULL,
+  `reminder` enum('Y','N') NOT NULL DEFAULT 'N',
+  `reminder_when` datetime DEFAULT NULL,
+  PRIMARY KEY (`sptb_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table my.company.support_packs_to_businesses: ~0 rows (approximately)
+DELETE FROM `support_packs_to_businesses`;
+/*!40000 ALTER TABLE `support_packs_to_businesses` DISABLE KEYS */;
+/*!40000 ALTER TABLE `support_packs_to_businesses` ENABLE KEYS */;
+
+
+-- Dumping structure for table my.company.tasks
+DROP TABLE IF EXISTS `tasks`;
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `task_id` int(10) NOT NULL AUTO_INCREMENT,
+  `assign_to` varchar(255) DEFAULT NULL,
+  `task_type_id` varchar(255) DEFAULT NULL,
+  `start` datetime DEFAULT NULL,
+  `est_completion` varchar(255) DEFAULT NULL,
+  `task` varchar(255) DEFAULT NULL,
+  `notes` varchar(255) DEFAULT NULL,
+  `complete` enum('Y','N') DEFAULT NULL,
+  `project_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`task_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table my.company.tasks: ~0 rows (approximately)
+DELETE FROM `tasks`;
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+
+
+-- Dumping structure for table my.company.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(10) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `display_name` varchar(255) DEFAULT NULL,
+  `group` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table my.company.users: ~0 rows (approximately)
+DELETE FROM `users`;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+
+-- Dumping structure for table my.company.websites
+DROP TABLE IF EXISTS `websites`;
+CREATE TABLE IF NOT EXISTS `websites` (
+  `website_id` int(10) NOT NULL AUTO_INCREMENT,
+  `business_id` int(10) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`website_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table my.company.websites: ~0 rows (approximately)
+DELETE FROM `websites`;
+/*!40000 ALTER TABLE `websites` DISABLE KEYS */;
+/*!40000 ALTER TABLE `websites` ENABLE KEYS */;
+/*!40014 SET FOREIGN_KEY_CHECKS=1 */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
