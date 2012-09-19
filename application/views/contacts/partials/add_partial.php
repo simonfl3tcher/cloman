86922a9d732687c52b25ee02bc3a1d1cf840e9d4
Work Computer <simonfletcher0@gmail.com>
2012-09-19 15:01:01 +0100

    updates to the modal when adding a customer

--- application/views/contacts/partials/add_partial.php
+++ application/views/contacts/partials/add_partial.php
@@ -1,96 +1,113 @@
-<form action="" method="post" id="addContactForm" data-ajaxurl='contacts/add' data-useAjax='true' class="form-horizontal">	
-<div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" class="modal hide fade" id="myModal" style="display: none;">
-            <div class="modal-header">
-              <button aria-hidden="true" data-dismiss="modal" class="close" type="button">X</button>
-              <h3 id="myModalLabel">Add A Contact</h3>
-            </div>
-            <div class="modal-body">				
-					<table class="std">
-						<tbody>
-							<tr class="largeField">
-								<td>
-									<label for="contact_Name_First" class="above">Name</label><br>
-									<span><input type="text" value="" id="contact_Name_First" name="contact[Name]"></span>
-								</td>
-								<td>
-									<label for="contact_Name_Last" class="above">Role</label><br>
-									<span><input type="text" value="" id="contact_Role" name="contact[Role]"></span>
-								</td>
-							</tr>
-							<tr class="largeField">
-								<td>
-									<label for="contact_Email" class="above">Email Address</label><br>
-									<span><input type="text" id="contact_Email" name="contact[Email]"></span>
-								</td>
-								<td>
-									<label for="contact_Url" class="above">Phone</label><br>
-									<span><input type="text" id="contact_Phone" name="contact[Phone]"></span>
-								</td>
-							</tr>
+	<form action="" method="post" id="addContactForm" data-ajaxurl='contacts/add' data-useAjax='true' class="form-horizontal">	
+	<div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" class="modal hide fade" id="myModal" style="display: none;">
+	            <div class="modal-header">
+	              <button aria-hidden="true" data-dismiss="modal" class="close" type="button">X</button>
+	              <span class="icon contactIcon"></span>
+	            </div>
+	            <div class="modal-body">		
+	            	<div class="addWrapper">		
+						<table class="std">
+							<tbody>
+								<tr class="largeField">
+									<td>
+										<label for="contact_Name_First" class="above">Name</label>
+										<span><input type="text" value="" id="contact_Name_First" name="contact[Name]"></span>
+									</td>
+									<td>
+										<label for="contact_Name_Last" class="above">Role</label>
+										<span><input type="text" value="" id="contact_Role" name="contact[Role]"></span>
+									</td>
+								</tr>
+								<tr class="largeField">
+									<td>
+										<label for="contact_Email" class="above">Email Address</label>
+										<span><input type="text" id="contact_Email" name="contact[Email]"></span>
+									</td>
+									<td>
+										<label for="contact_Url" class="above">Phone</label>
+										<span><input type="text" id="contact_Phone" name="contact[Phone]"></span>
+									</td>
+								</tr>
+								<tr>
+									<td colspan="2">
+										<label for="contact_Url" class="above">Notes</label>
+										<textarea id="contact_Phone" name="contact[Notes]" rows="4" cols="20"></textarea>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						<!-- This needs to be hidden until they say to add a business. -->
+						<div class="stdpadh stdpadt">
+							<span>Business</span>
+						</div>
+						
+						<table class="std">
 							<tr>
-								<td colspan="2">
-									<label for="contact_Url" class="above">Notes</label><br>
-									<textarea id="contact_Phone" name="contact[Notes]" rows="4" cols="20"></textarea>
+								<td><input id="my-text-input" type="text" name="business[Current_2]" class="selectBusiness" /></td>
+								<td>
+									<div class="addBusiness icon plusIcon"></div>
 								</td>
 							</tr>
-						</tbody>
-					</table>
-					<!-- This needs to be hidden until they say to add a business. -->
-					<div class="stdpadh stdpadt">
-						<h3>Business</h3>
-						<p>Please add the information bellow to asign a business to the person.</p>
+						</table>
+
+						<div class="businessForm">
+							<div class="paddingTop paddingBottom">
+								<span class="icon busniessIcon"></span>
+								<span class="addBusinessTitle"><h6>Add a new business</h6></span>
+							</div>
+							<table class="std ">
+								<tr class="largeField">
+									<td>
+										<label for="business_name" class="above">Business Name</label>
+										<span><input type="text" value="" id="business_name" name="business[Name]"></span>
+									</td>
+									<td>
+										<label for="business_name">Email</label>
+										<span><input type="text" value="" id="business_name" name="business[Email]"></span>
+									</td>
+								</tr>
+								<tr class="largeField">
+									<td>
+										<label for="business_name">Phone</label>
+										<span><input type="text" value="" id="business_name" name="business[Phone]"></span>
+									</td>
+								</tr>
+								<tr class="largeField">
+									<td>
+										<label for="address_Address_Line_1">Property Name / Number*</label>
+										<span><input type="text" value="" id="address_Address_Line_1" name="address[Address_Line_1]"></span>
+									</td>
+									<td>
+										<label for="address_Address_Line_2">Street</label>
+										<span><input type="text" value="" id="address_Address_Line_2" name="address[Address_Line_2]"></span>
+									</td>
+								</tr>
+								<tr class="largeField">
+									<td>
+										<label for="address_Address_Line_3">Area</label>
+										<span><input type="text" value="" id="address_Address_Line_3" name="address[Address_Line_3]"></span>
+									</td>
+									<td>
+										<label for="address_City">Town / City*</label>
+										<span><input type="text" value="" id="address_City" name="address[City]"></span>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<label for="address_Postcode">Postcode*</label>
+										<span><input type="text" value="" id="address_Postcode" name="address[Postcode]"></span>
+									</td>
+								</tr>
+							</tbody>
+						</table>
 					</div>
-					
-					<table class="std">
-						<tr>
-							<td><input id="my-text-input" type="text" name="business[Current_2]" class="selectBusiness" /></td>
-							<td>
-								<p class="addBusiness">Add a new business</p>
-							</td>
-						</tr>
-					</table>
-					<div class="businessForm">
-						<table class="std ">
-							<tr class="largeField">
-								<td><label for="business_name">Business Name</label></td>
-								<td><span><input type="text" value="" id="business_name" name="business[Name]"></span></td>
-							</tr>
-							<tr class="largeField">
-								<td><label for="business_name">Email</label></td>
-								<td><span><input type="text" value="" id="business_name" name="business[Email]"></span></td>
-							</tr>
-							<tr class="largeField">
-								<td><label for="business_name">Phone</label></td>
-								<td><span><input type="text" value="" id="business_name" name="business[Phone]"></span></td>
-							</tr>
-							<tr class="largeField">
-								<td><label for="address_Address_Line_1">Property Name / Number*</label></td>
-								<td><span><input type="text" value="" id="address_Address_Line_1" name="address[Address_Line_1]"></span></td>
-							</tr>
-							<tr class="largeField">
-								<td><label for="address_Address_Line_2">Street</label></td>
-								<td><span><input type="text" value="" id="address_Address_Line_2" name="address[Address_Line_2]"></span></td>
-							</tr>
-							<tr class="largeField">
-								<td><label for="address_Address_Line_3">Area</label></td>
-								<td><span><input type="text" value="" id="address_Address_Line_3" name="address[Address_Line_3]"></span></td>
-							</tr>
-							<tr class="largeField">
-								<td><label for="address_City">Town / City*</label></td>
-								<td><span><input type="text" value="" id="address_City" name="address[City]"></span></td>
-							</tr>
-							<tr>
-								<td><label for="address_Postcode">Postcode*</label></td>
-								<td><span><input type="text" value="" id="address_Postcode" name="address[Postcode]"></span></td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
+			</div>
 		</div>
-            <div class="modal-footer">
-            	<div class="resetForm"></div>
-              <button data-dismiss="modal" type="reset" class="btn">Close</button>
-              <button data-dismiss="modal" type="submit" class="btn btn-primary">Save Contact</button>
-            </div>
-          </div>
-        </form>
+		    <div class="modal-footer">
+		    	<div class="resetForm icon resetIcon"></div>
+		      <button data-dismiss="modal" type="reset" class="btn">Close</button>
+		      <button data-dismiss="modal" type="submit" class="btn btn-primary">Save Contact</button>
+		    </div>
+  		</div>
+	</form>
+	<div class="clear"></div>
